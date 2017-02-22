8f427bd77e6611943e76d671a9de423bb36a14c5
Meltorn <sm407@mail.ru>
2017-02-22 14:39:58 +0300

    script

---
+++ js/script.js
@@ -0,0 +1,63 @@
+// Напишите нам
+    var link = document.querySelector(".mail-button");
+    var popup = document.querySelector(".write-us");
+    var close = popup.querySelector(".write-us-userdata a");
+   
+    link.addEventListener("click", function(event) {
+      event.preventDefault();
+      popup.classList.add("write-content-show");
+    });
+    close.addEventListener("click", function(event) {
+      event.preventDefault();
+      popup.classList.remove("write-content-show");
+    });
+    window.addEventListener("keydown", function(event) {
+        if (event.keyCode === 27) {
+          if (popup.classList.contains("write-content-show")) {
+            popup.classList.remove("write-content-show");
+          }
+        }
+      });
+
+  
+ //Карта
+    var mapLink = document.querySelector(".js-open-map");
+    var mapPopup = document.querySelector(".map-popup");
+    var mapClose = document.querySelector(".map-popup a");
+   
+    mapLink.addEventListener("click", function(event) {
+      event.preventDefault();
+      mapPopup.classList.add("map-content-show");
+    });
+    mapClose.addEventListener("click", function(event) {
+      event.preventDefault();
+      mapPopup.classList.remove("map-content-show");
+    });
+    window.addEventListener("keydown", function(event) {
+        if (event.keyCode === 27) {
+          if (mapPopup.classList.contains("map-content-show")) {
+            mapPopup.classList.remove("map-content-show");
+          }
+        }
+      });
+
+//Товар добавлен
+    var addlink = document.querySelector(".buy");
+    var addpopup = document.querySelector(".product-add");
+    var addclose = document.querySelector(".product-add-close");
+   
+    addlink.addEventListener("click", function(event) {
+      event.preventDefault();
+      addpopup.classList.add("product-content-show");
+    });
+    addclose.addEventListener("click", function(event) {
+      event.preventDefault();
+      addpopup.classList.remove("product-content-show");
+    });
+    window.addEventListener("keydown", function(event) {
+        if (event.keyCode === 27) {
+          if (addpopup.classList.contains("product-content-show")) {
+            addpopup.classList.remove("product-content-show");
+          }
+        }
+      });
